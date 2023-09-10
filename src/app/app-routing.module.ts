2d092795ed00e779f0529a34449e09e86e28c8e1
octavio2150 <octavio2150@gmail.com>
2023-09-10 16:21:01 -0500

    actividad6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { MisionComponent } from './mision/mision.component';
 
-const routes: Routes = [];
 
+
+
+const routes: Routes = [
+{path:"",component:HomeComponent, pathMatch:"full"},
+{path:"home",component:HomeComponent, pathMatch:"full"},
+{path:"mision",component:MisionComponent, pathMatch:"full"},
+{path:"login",component:LoginComponent, pathMatch:"full"},
+{path:"registro",component:RegistroComponent, pathMatch:"full"},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { MisionComponent } from './mision/mision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistroComponent,
+    MisionComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
